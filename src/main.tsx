c9f1c2e251d2e57b29b9dc264528c7f71952c9d5
Techypanda <Jonathan_wright@hotmail.com>
2022-04-22 22:12:54 +0800

    Update to MUI

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
+
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const container = document.getElementById("root");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
 
+const theme = createTheme({
+  typography: {
+    fontFamily: "'IBM Plex Sans', sans-serif",
+  }
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
